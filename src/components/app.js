4925a524a1099bb3e8055c43dc88c14e76ef17c2
Hack Reactor Student <student@hackreactor.com>
2017-09-01 12:29:42 -0500

    angularjs rendering default data on page

--- src/components/app.js
+++ src/components/app.js
@@ -2,4 +2,7 @@ angular.module('video-player')
 
 .component('app', {
   // TODO
+  templateUrl: 'src/templates/app.html',
+}).controller('appControl', function($scope){
+  $scope.videos = window.exampleVideoData
 });
